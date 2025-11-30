fe59126ca148ee257a2fc9aa5452b5446dec2d13
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-11-30 22:37:44 +0100

    Configure buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,8 +2,11 @@ plugins {
     `kotlin-dsl`
 }
 
+// This sets the Java toolchain used to run Gradle tasks (here Java 17). Thanks to the Foojay plugin, Gradle will
+// automatically discover or download the required JDK if it's missing. This does NOT affect the Java version of the
+// application code, which can be higher (e.g., Java 25).
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    toolchain.languageVersion = JavaLanguageVersion.of(17)
 }
 
 repositories {
