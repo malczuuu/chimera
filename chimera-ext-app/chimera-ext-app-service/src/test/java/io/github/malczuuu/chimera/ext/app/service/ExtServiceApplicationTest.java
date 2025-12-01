b3b2317c89afa11c3db7867377d1291801082fa5
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-01 20:55:26 +0100

    Update ExtServiceApplicationTest

--- chimera-ext-app/chimera-ext-app-service/src/test/java/io/github/malczuuu/chimera/ext/app/service/ExtServiceApplicationTest.java
+++ chimera-ext-app/chimera-ext-app-service/src/test/java/io/github/malczuuu/chimera/ext/app/service/ExtServiceApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles({"test"})
-@SpringBootTest
+@SpringBootTest(classes = {ExtServiceApplication.class})
 @PostgresContainerTest
 class ExtServiceApplicationTest {
 
