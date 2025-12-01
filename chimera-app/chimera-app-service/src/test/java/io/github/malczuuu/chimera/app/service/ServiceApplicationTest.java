7d3adfdebba0e8b73de0143e509a343d9a0cc3d0
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-01 20:53:51 +0100

    Update ServiceApplicationTest

--- chimera-app/chimera-app-service/src/test/java/io/github/malczuuu/chimera/app/service/ServiceApplicationTest.java
+++ chimera-app/chimera-app-service/src/test/java/io/github/malczuuu/chimera/app/service/ServiceApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles({"test"})
-@SpringBootTest
+@SpringBootTest(classes = {ServiceApplication.class})
 @PostgresContainerTest
 class ServiceApplicationTest {
 
