e13e5ac893203bc7d00add6d20b4bee0f7e1b818
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-01 20:54:17 +0100

    Remove duplicated test

--- chimera-app/chimera-migration/src/test/java/io/github/malczuuu/chimera/migration/MigrationTestApplication.java
+++
@@ -1,8 +0,0 @@
-package io.github.malczuuu.chimera.migration;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
-@EnableAutoConfiguration
-@SpringBootConfiguration
-public class MigrationTestApplication {}
