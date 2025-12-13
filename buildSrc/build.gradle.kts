ecbd088af96aa06841d98efd8e3b4169eb3cafc4
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-13 17:01:02 +0100

    Update Gradle setup

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,17 +2,15 @@ plugins {
     `kotlin-dsl`
 }
 
-// This is for buildSrc module only. Current toolchain is set to Java 17 here as a minimum
-// supported version by Gradle 9. Other modules can use higher Java version (e.g., 21 or 25). Local
-// Java installation may also be different, as long as it's higher than 17.
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
+// Kotlin does not yet support 25 JDK target, to be revisited in the future.
+if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_25)) {
+    kotlin {
+        jvmToolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
 }
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    implementation("org.eclipse.jgit:org.eclipse.jgit:7.4.0.202509020913-r")
 }
